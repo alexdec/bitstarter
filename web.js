390ada2929f2e95dfe9e54cbae3b9bd2ca74e2ff
alexdec <prettymiserable@googlemail.com>
2013-07-17 18:09:55 +0200

    Update web.js

--- web.js
+++ web.js
@@ -2,9 +2,8 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
-app.get('/', function(request, response) {
-  response..open('index.html');
-});
+var msg = fs.readFileSync('index.html','utf8');
+console.log(msg);
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
