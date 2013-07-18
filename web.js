46ef125f0ca7f3b41b64741e9e946fb565d6fa6f
alexdec <prettymiserable@googlemail.com>
2013-07-18 19:29:11 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,5 +1,14 @@
+var express = require('express');
 var fs = require('fs');
-var buf = fs.readFileSync('index.html');
-var msg = buf.toString();
+var htmlfile = "index.html";
+var app = express.createServer(express.logger());
 
-console.log(msg);
+app.get('/', function(request, response) {
+    var html = fs.readFileSync(htmlfile).toString();
+    response.send(html);
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port,function() {
+  console.log("Listening on " + port);
+});
